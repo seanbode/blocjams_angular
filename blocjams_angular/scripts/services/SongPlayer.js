4a6a15bd794868fa78b326584e400d530655aff8
Sean Bode <seanpaulbode@gmail.com>
2017-04-20 19:16:35 -0400

    player bar

--- blocjams_angular/scripts/services/SongPlayer.js
+++ blocjams_angular/scripts/services/SongPlayer.js
@@ -1,8 +1,14 @@
 (function() {
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
       var SongPlayer = {};
 
-      var currentSong = null;
+      var currentAlbum = Fixtures.getAlbum();
+
+      var getSongIndex = function(song) {
+        return currentAlbum.songs.indexOf(song);
+      };
+
+      SongPlayer.currentSong = null;
       /**
       * @desc Buzz object audio file
       * @type {Object}
@@ -35,15 +41,34 @@
       };
 
       SongPlayer.play = function(song) {
+        song = song || SongPlayer.currentSong;
         setSong(song);
       };
       SongPlayer.pause = function(song) {
+        song = song || SongPlayer.currentSong;
         currentBuzzObject.pause();
         song.playing = false;
       };
+
+      SongPlayer.previous = function() {
+        var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+        currentSongIndex--;
+
+        if (currentSongIndex < 0) {
+          currentBuzzObject.stop();
+          SongPlayer.currentSong.playing = null;
+        } else {
+          var song = currentAlbum.songs[currentSongIndex];
+          setSong(song);
+          playSong(song);
+        }
+      };
+
+
+
       return SongPlayer;
     }
     angular
       .module('blocJams')
-      .factory('SongPlayer', SongPlayer);
+      .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
