22fdbee19a7d486aa6899ce596c5110bea0dbaec
Sean Bode <seanpaulbode@gmail.com>
2017-05-23 14:13:42 -0400

    metrics chartist

--- blocjams_angular/scripts/controllers/MetricsCtrl.js
+++ blocjams_angular/scripts/controllers/MetricsCtrl.js
@@ -2,11 +2,98 @@ angular
   .module('blocJams')
   .controller('MetricsCtrl', function ($scope, $rootScope, Metrics) {
     $scope.metric = { chart: Metrics.chart }
+
+    $scope.bardata = {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      series:
+      [
+        [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+        [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+      ]
+    }
     $scope.buttonCallback = function() {
       alert("button clicked");
       Metrics.listSongsPlayed()
     };
-    $scope.data = [
-      {song: "blue", plays: 5 }
-    ]
+    // $scope.data = [
+    //   {song: "blue", plays: 5 }
+    // ]
+  }).directive('pieChart', function() {
+    return {
+			restrict: 'EA',
+			template: '<div class="ct-chart ct-perfect-fourth"></div>',
+			scope: {
+					data: '='
+			},
+			link: function(scope, elem, attr) {
+				var chartElement = elem[0].querySelector('.ct-chart');
+				var options = {
+					donut: true,
+					donutWidth: 80
+				};
+
+				var chart = new Chartist.Pie(chartElement, {
+					labels: [],
+					series: []
+				}, options).on('draw', draw).on('created', created);
+
+
+				scope.$watch('data', function(newValue, oldValue) {
+						chart.update({
+						labels: [],
+						series: scope.data
+					});
+				}, true);
+
+				var oldBars = [];
+
+				var lastValues = [];
+				function created() {
+					lastValues = angular.copy(scope.data);
+				}
+
+				function draw(data) {
+					if (angular.equals(lastValues, scope.data))
+						// no animation if nothing changed
+						return;
+					if(data.type === 'slice') {
+              // Calculate the dureation so that all slices at same speed
+              var angle = data.endAngle - data.startAngle;
+              var dur = angle / 360 * 1000;
+
+              // Get the total path length in order to use for dash array animation
+              var pathLength = data.element._node.getTotalLength();
+
+              // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+              data.element.attr({
+                'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+              });
+              console.log(data);
+              // Create animation definition while also assigning an ID to the animation for later sync usage
+              var animationDefinition = {
+                'stroke-dashoffset': {
+                  id: 'anim' + data.index,
+                  dur: dur,
+                  from: -pathLength + 'px',
+                  to:  '0px',
+                  // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+                  fill: 'freeze'
+                }
+              };
+
+              // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+              if(data.index !== 0) {
+                animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+              }
+
+              // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+              data.element.attr({
+                'stroke-dashoffset': -pathLength + 'px'
+              });
+
+              data.element.animate(animationDefinition, false);
+            }
+        }
+    	}
+		}
   });
--- blocjams_angular/scripts/services/Metrics.js
+++ blocjams_angular/scripts/services/Metrics.js
@@ -15,34 +15,6 @@
           $window.localStorage.songPlays = JSON.stringify(plays)
         };
       },
-      chart: {
-        type: "pieChart",
-        height: 500,
-        x: function (d){return d.song;},
-        y: function (d){return d.plays;},
-        showLabels: true,
-        duration: 500,
-        labelThreshold: 0.01,
-        labelSunbeamLayout: true,
-        legend: {
-          margin: {
-            top: 5,
-            right: 35,
-            bottom: 5,
-            left: 0
-          }
-        }
-      },
-      title: {
-        enable: true,
-        text: "Songs Played",
-        className: "h4",
-        css:
-        {
-          width: 0,
-          textAlign: "center"
-        }
-      },
       listSongsPlayed: function() {
         var songs = [];
         angular.forEach(JSON.parse($window.localStorage.songPlays), function(song) {
